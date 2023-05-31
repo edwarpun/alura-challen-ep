417defe1f4b03668f9b651eb610d8db8e89b7763
Edwar <edwarpun@hotmail.com>
2023-05-30 23:30:39 -0400

    Commit programa encriptador de texto

---
+++ index.js
@@ -0,0 +1,79 @@
+function Encriptar() {
+ 
+  console.log(document.getElementById("texto").value);
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./imagenes/encriptado.jpg";
+
+  
+
+  } else {
+    muñeco.src = "./imagenes/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
+}
+
+function Desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./imagenes/desencriptado.jpg";
+    } else {
+      muñeco.src = "./imagenes/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Oops!", "Debes ingresar un texto", "warning");
+    }
+}
+
+
+
+
+function handleButtonClick() {
+  Encriptar();
+}
+
+
+function handleButtonClickDesc() {
+  Desencriptar();
+}
+
+// Get the button element
+var encriptarButton = document.getElementById("_encriptar");
+var desencriptarButton = document.getElementById("_desencriptar");
+
+// Attach the click event listener to the buttons
+encriptarButton.addEventListener("click", handleButtonClick);
+desencriptarButton.addEventListener("click", handleButtonClickDesc);
+
+
+
